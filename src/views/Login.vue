f2912fec9a1a7a586f9752e326cbd6425436ef83
Hamimi Zouaoui <zouaoui.hm@gmail.com>
2022-02-08 16:17:25 +0100

    fixed synthax

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -73,7 +73,7 @@ export default {
 
                     this.$router.push('/home')
                 }
-            } catch () {
+            } catch (error) {
                 this.loginError = "Connexion échouée"
             }
         }
